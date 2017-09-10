befda06ae102741c42d3f2bad9eaf4a24143ffab
Kyle Rader <kyle@kylerader.ninja>
2017-09-10 08:41:08 -0700

    Initial Commit: mix new plug_web_proxy

---
+++ lib/plug_web_proxy.ex
@@ -0,0 +1,18 @@
+defmodule PlugWebProxy do
+  @moduledoc """
+  Documentation for PlugWebProxy.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugWebProxy.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plug_web_proxy_test.exs
@@ -0,0 +1,8 @@
+defmodule PlugWebProxyTest do
+  use ExUnit.Case
+  doctest PlugWebProxy
+
+  test "greets the world" do
+    assert PlugWebProxy.hello() == :world
+  end
+end
